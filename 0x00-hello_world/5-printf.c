a5f76efece7a8cfca6e866324be3755bc97f854d
kristofaumeh <xtopherumeh@gmail.com>
2023-07-15 01:11:18 +0100

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Print command on the screen
+ * using printf not puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+		return (0);
+
+}
