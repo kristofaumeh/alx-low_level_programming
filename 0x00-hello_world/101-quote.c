d8f0e452c53c3271aae45007241c8a7a711e692b
kristofaumeh <xtopherumeh@gmail.com>
2023-07-15 21:49:59 +0100

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,12 @@
   * main - Entry point
   * main print an output without printf and puts
   *
-  * Return: (1)
+  * Return: Always 1 (Success)
   */
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n",
-				  59);
+		write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n",
+				59);
 		return (1);
 }
 
