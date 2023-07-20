13303e983b01045a9831b85fb7cdd7bdb6ff87d9
kristofaumeh <xtopherumeh@gmail.com>
2023-07-20 14:12:23 +0100

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point-alphabet
+ * Return Always 0 (success)
+ */
+
+
+void print_alphabet(void)
+{
+		char alphabet;
+
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
+{
+		_putchar(alphabet);
+}
+		_putchar('\n');
+}
