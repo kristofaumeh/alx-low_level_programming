e440d3b53b63927176be9d442fe8d1069fe998c4
kristofaumeh <xtopherumeh@gmail.com>
2023-08-11 14:17:47 +0100

    alloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,29 +14,28 @@ int **alloc_grid(int width, int height)
 
 		if (width <= 0 || height <= 0)
 		{
-		return (NULL);
+				return (NULL);
 		}
 		gridout = malloc(height * sizeof(int *));
 		if (gridout == NULL)
 		{
-		free(gridout);
-		return (NULL);
+				free(gridout);
+				return (NULL);
 		}
 		for (i = 0; i < height; i++)
 		{
-		gridout[i] = malloc(width * sizeof(int));
-		}
-		if (gridout[i] == NULL)
-		{
-		for (i--; i >= 0; i--)
-
-		free(gridout[i]);
-		free(gridout);
-		return (NULL);
+				gridout[i] = malloc(width * sizeof(int));
+				if (gridout[i] == NULL)
+				{
+						for (i--; i >= 0; i--)
+						free(gridout[i]);
+						free(gridout);
+						return (NULL);
+				}
 		}
 		for (i = 0; i < height; i++)
-		for (j = 0; j < width; j++)
-		gridout[i][j] = 0;
+				for (j = 0; j < width; j++)
+						gridout[i][j] = 0;
 
 		return (gridout);
 }
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * free_grid: frees a 2 dimensional grid previously created
+  * @grid: two dimensional grid
+  * @height: height of grid
+  * Description: frees memory of grid
+  * Return: 0;
+  */
