8dfc1617e695274dd9c91cb9accb378bd5441df6
kristofaumeh <xtopherumeh@gmail.com>
2023-08-14 17:07:49 +0100

    sum

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef SUM_H
+#define SUM_H
+/**
+* SUM(x, y) - a function like macro that computes the sum of two numbers
+* @x: input value
+* @y: input value
+* Description: a macro function
+*/
+#define SUM(x, y)  ((x) + y)
+
+#endif
