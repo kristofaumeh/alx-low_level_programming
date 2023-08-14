1a979dcee0f48152395d7238f4b5d0bc0309f059
kristofaumeh <xtopherumeh@gmail.com>
2023-08-14 16:56:35 +0100

    function like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef MACROL_H
+#define MACROL_H
+/**
+* Filename: 3-function_like_macro
+* @x: input value
+* Description: a header file that computes absolute value
+*/
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
+
+#endif
