063835187b6ea53c639426d26873af51dad28cc8
kristofaumeh <xtopherumeh@gmail.com>
2023-08-16 00:02:11 +0100

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+* struct dog - a struct that creates a dog information
+* @name: dog's name
+* @age: dog's age
+* @owner: owner's name
+* Description: user struct file
+*/
 
 struct dog
 {
