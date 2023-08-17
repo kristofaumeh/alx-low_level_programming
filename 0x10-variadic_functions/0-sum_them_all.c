e76500c0c12450341373b7335e87ff6cf7ae62a1
kristofaumeh <xtopherumeh@gmail.com>
2023-08-17 15:22:22 +0100

    sum_them_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,14 +15,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (n == 0)
-		{
-			return (0);
-		}
-		else
-		{
-			sum += va_arg(place, int);
-		}
+		sum += va_arg(place, int);
 	}
 	va_end(place);
 	return (sum);
