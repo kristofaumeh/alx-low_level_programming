5c78b99be9f2c8d5614c43aefd66948a0c995a4d
kristofaumeh <xtopherumeh@gmail.com>
2023-08-28 22:38:25 +0100

    free -listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+* free-listint2 - A function that frees all nodes in a list and
+* returns the head to null
+* @head: address to the first node on the list
+*
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+}
